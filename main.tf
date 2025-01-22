26d12879687179a097b39a41a407556135359f72
jason1980p <jason1980p@gmail.com>
2025-01-22 13:02:40 -0500

    Create new dev vpc

--- main.tf
+++ main.tf
@@ -13,27 +13,46 @@ data "aws_ami" "app_ami" {
 
   owners = ["979382823631"] # Bitnami
 }
+# default VPC
 data "aws_vpc" "default" {
   default = true
 }
+# New VPC module
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
 
 
+# EC2 module
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
+  subnet_id = module.blog_vpc.public_subnets[0]
+
   tags = {
     Name = "HelloWorld"
   }
 }
-
+# Security Group module
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
   name    = "blog"
-  vpc_id  = data.aws_vpc.default.id
+  vpc_id  = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
